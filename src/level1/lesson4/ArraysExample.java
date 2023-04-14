3bc356bb74ffe1f923f89fa11b11b2f534ca9e0a
Serzas <j_sergej@yahoo.com>
2023-04-14 18:56:42 +0300

    infexOf added

--- src/level1/lesson4/ArraysExample.java
+++ src/level1/lesson4/ArraysExample.java
@@ -10,7 +10,7 @@ public class ArraysExample {
         String [] age = {"12", "4", "5", "2", "5"};
 
         int getArrayIndex = Arrays.asList(age).indexOf("4");
-        System.out.println("Index of 12: " + getArrayIndex);
+        System.out.println("Index of 4: " + getArrayIndex);
 
         // access each array elements
         System.out.println("Accessing Elements of Array:");
