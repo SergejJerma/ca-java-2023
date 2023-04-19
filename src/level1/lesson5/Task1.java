092cd25e33dca3ad2a2c3ad33254e7294a32abde
Serzas <j_sergej@yahoo.com>
2023-04-19 20:00:36 +0300

    lesson5 task1 added

---
+++ src/level1/lesson5/Task1.java
@@ -0,0 +1,21 @@
+package level1.lesson5;
+
+public class Task1 {
+    public static void main(String[] args) {
+        m1();
+    }
+
+    public static void m1() {
+        System.out.println("first");
+        m2();
+    }
+
+    public static void m2() {
+        System.out.println("second");
+        m3();
+    }
+
+    public static void m3() {
+        System.out.println("third");
+    }
+}
