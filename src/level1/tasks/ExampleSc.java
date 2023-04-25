bdb56e50a5e3230430d4def3ea14f062061189d7
Serzas <j_sergej@yahoo.com>
2023-04-25 21:55:13 +0300

    string format tasks code added

---
+++ src/level1/tasks/ExampleSc.java
@@ -0,0 +1,12 @@
+package level1.tasks;
+
+import java.util.Scanner;
+
+public class ExampleSc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter double");
+        Double num = sc.nextDouble();
+        System.out.println(num);
+    }
+}
