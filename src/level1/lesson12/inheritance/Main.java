fd7407ed559216441acf022e325224b695b2cc9c
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-04-28 21:55:11 +0300

    Show abstraction (using parent class instead of child)

--- src/level1/lesson12/inheritance/Main.java
+++ src/level1/lesson12/inheritance/Main.java
@@ -12,12 +12,14 @@ public class Main {
         Studentas studentas = new Studentas("Antanas", "Antanaitis", "39901011111", "123456");
         System.out.println(String.format("Studentas: %s %s %s %s", studentas.getVardas(), studentas.getPavarde(), studentas.asmensKodas,
                 studentas.getStudentoPazymejimoNr()));
+        doSomething(studentas);
 
         System.out.println(studentas.getInfo());
 
         Darbuotojas darbuotojas = new Darbuotojas("Mantas", "Mantauskas", "39901012222", "333");
         System.out.println(String.format("Darbuotojas: %s %s %s %s", darbuotojas.getVardas(), darbuotojas.getPavarde(), darbuotojas.asmensKodas,
                 darbuotojas.getSocDraudimoNr()));
+        doSomething(darbuotojas);
 
         System.out.println(darbuotojas.getInfo());
 
@@ -30,6 +32,20 @@ public class Main {
 
 
         ImonesXDarbuotojas xDarbuotojas2 = new ImonesXDarbuotojas(darbuotojas, "xNr");
+        xDarbuotojas2.getxNr(); //ImonesXDarbuotojas turi getxNr()
+        doSomething(xDarbuotojas2);
 
+        Asmuo imonesDarbuotojasIrgiYraAsmuo = new ImonesXDarbuotojas(darbuotojas, "xNr");
+        imonesDarbuotojasIrgiYraAsmuo = darbuotojas;
+        /* Prieinami tik asmens metodai! */
+        //imonesDarbuotojasIrgiYraAsmuo.getxNr(); //ImonesXDarbuotojas turi getxNr()
+        doSomething(imonesDarbuotojasIrgiYraAsmuo);
     }
+
+    private static void doSomething(Asmuo asmuo) {
+        System.out.printf("Metodas doSomething iskviestas argumenta, priklausanti klasei %s%n", asmuo.getClass().getName());
+        asmuo.getInfo();
+    }
+
+
 }
