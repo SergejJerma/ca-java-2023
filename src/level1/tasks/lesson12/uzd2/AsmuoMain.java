470db17a0225544bbab693bb96d399685d337487
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-05-02 22:03:12 +0300

    Lesson12.uzd2 - spausdinam asmenu sarasa

--- src/level1/tasks/lesson12/uzd2/AsmuoMain.java
+++ src/level1/tasks/lesson12/uzd2/AsmuoMain.java
@@ -1,11 +1,20 @@
 package level1.tasks.lesson12.uzd2;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AsmuoMain {
     public static void main(String[] args) {
         Asmuo asmuo = new Asmuo("vardenis", "pavardenis");
         System.out.println(asmuo);
 
+
+        List<Asmuo> asmenuSarasas = new ArrayList<>();
+        asmenuSarasas.add(asmuo);
+        asmenuSarasas.add(new Asmuo("vardenis2", "pavardenis2"));
+
+        System.out.println(asmenuSarasas);
 
 
     }
