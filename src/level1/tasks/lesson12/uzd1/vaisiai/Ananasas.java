e661c892e258444e175923cb636b7706d8d2d6b7
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-05-02 19:44:25 +0300

    Lesson12.uzd1 - kuriam klases

---
+++ src/level1/tasks/lesson12/uzd1/vaisiai/Ananasas.java
@@ -0,0 +1,4 @@
+package level1.tasks.lesson12.uzd1.vaisiai;
+
+public class Ananasas {
+}
---
+++ src/level1/tasks/lesson12/uzd1/vaisiai/VaisiaiUzd1.java
@@ -0,0 +1,16 @@
+package level1.tasks.lesson12.uzd1.vaisiai;
+
+public class VaisiaiUzd1 {
+    public static void main(String[] args) {
+        Vaisius vaisius = new Vaisius();
+        EgzotinisVaisius egzotinisVaisius = new EgzotinisVaisius();
+        Ananasas ananasas = new Ananasas();
+        Mangas mangas = new Mangas();
+        LietuviskasVaisius lietuviskasVaisius = new LietuviskasVaisius();
+        Kriause kriause = new Kriause();
+        Obuolys obuolys = new Obuolys();
+        Alyvinis alyvinis = new Alyvinis();
+        Antaninis antaninis = new Antaninis();
+
+    }
+}
