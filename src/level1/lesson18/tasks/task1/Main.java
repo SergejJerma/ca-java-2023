c55f209a1d08620ba23a469b08499d869055fff3
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-05-10 21:35:02 +0300

    Lesson18.task1 - nuskaitom varda is failo

--- src/level1/lesson18/tasks/task1/Main.java
+++ src/level1/lesson18/tasks/task1/Main.java
@@ -1,5 +1,7 @@
 package level1.lesson18.tasks.task1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -7,6 +9,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         FileWriter fileWriter = new FileWriter("vardas.txt");
         fileWriter.write("Tomas Žvinys");
+        fileWriter.flush();
+
+        FileReader fileReader = new FileReader("vardas.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String vardas = bufferedReader.readLine();
+        System.out.println(vardas);
+
         fileWriter.close();
+        fileReader.close();
     }
+
 }
