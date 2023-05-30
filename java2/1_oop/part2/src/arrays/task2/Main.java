837762497d226c02b9970faf39585d91807c3346
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-05-30 19:10:49 +0300

    Java2.OOP.3 Arrays. Task2 filling with random numbers and printing a 2D array

--- java2/1_oop/part2/src/arrays/task2/Main.java
+++ java2/1_oop/part2/src/arrays/task2/Main.java
@@ -8,9 +8,36 @@ public class Main {
     static Random rand = new Random();
 
     public static void main(String[] args) {
+        initializeThenFillThenPrint();
+        fillWhenInitializing();
+        useArraysFill();
+    }
+
+    private static void useArraysFill() {
+        int[][] arr2d = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            int[] arr1d = new int[4];
+            Arrays.fill(arr1d, rand.nextInt(10));
+            arr2d[i] = arr1d;
+        }
+        System.out.println(Arrays.deepToString(arr2d));
+    }
+
+    private static void fillWhenInitializing() {
+        int[][] arr = {
+                {rand.nextInt(10), rand.nextInt(10), rand.nextInt(10), rand.nextInt(10)},
+                {rand.nextInt(10), rand.nextInt(10), rand.nextInt(10), rand.nextInt(10)},
+                {rand.nextInt(10), rand.nextInt(10), rand.nextInt(10), rand.nextInt(10)},
+                {rand.nextInt(10), rand.nextInt(10), rand.nextInt(10), rand.nextInt(10)}
+        };
+
+        System.out.println(Arrays.deepToString(arr));
+    }
+
+    private static void initializeThenFillThenPrint() {
         int[][] arr = new int[4][4];
         for (int i = 0; i < arr.length; i++)
-            for (int j  = 0; j < arr[i].length; j++)
+            for (int j = 0; j < arr[i].length; j++)
                 arr[i][j] = rand.nextInt(10);
 
         System.out.println(Arrays.deepToString(arr));
