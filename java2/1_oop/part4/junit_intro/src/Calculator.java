154842ab732f9962d22a6b0e0b60806940768097
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-05 18:39:49 +0300

    Java2.OOP.4 JUnit introduction

---
+++ java2/1_oop/part4/junit_intro/src/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public int add(Integer a, Integer b) {
+        if (a == null)
+            return b;
+        if (b == null)
+            return a;
+        return a + b;
+    }
+
+    public int multiply(Integer a, Integer b) {
+        if (a == null || b == null)
+            return 0;
+        return a * b;
+    }
+}
---
+++ java2/1_oop/part4/junit_intro/src/CalculatorTest.java
@@ -0,0 +1,84 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @org.junit.jupiter.api.Test
+    void add_withValidInput_providesValidOutput() {
+        //given
+        int a = 1;
+        int b = 2;
+
+        //when
+        int result = calculator.add(a, b);
+
+        //then
+        assertEquals(3, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void add_withNullBInput_providesValidOutput() {
+        //given
+        int a = 1;
+        Integer b = null;
+
+        //when
+        int result = calculator.add(a, b);
+
+        //then
+        assertEquals(1, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void add_withNullAInput_providesValidOutput() {
+        //given
+        int b = 1;
+        Integer a = null;
+
+        //when
+        int result = calculator.add(a, b);
+
+        //then
+        assertEquals(1, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply_withValidInput_providesValidOutput() {
+        //given
+        int a = 2;
+        int b = 3;
+
+        //when
+        int result = calculator.multiply(a, b);
+
+        //then
+        assertEquals(6, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply_withNullAInput_providesValidOutput() {
+        //given
+        Integer a = null;
+        int b = 3;
+
+        //when
+        int result = calculator.multiply(a, b);
+
+        //then
+        assertEquals(0, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply_withNullBInput_providesValidOutput() {
+        //given
+        int a = 2;
+        Integer b = null;
+
+        //when
+        int result = calculator.multiply(a, b);
+
+        //then
+        assertEquals(0, result);
+    }
+}
