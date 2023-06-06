72ecdd19be7dca1c41fa85947227798c5c285e2b
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-06 22:24:19 +0300

    Java2.OOP.5 Lambda - What is it? (refactored)

--- java2/1_oop/part5/lambdas/src/main/java/Main.java
+++ java2/1_oop/part5/lambdas/src/main/java/Main.java
@@ -7,34 +7,45 @@ public class Main {
         functionAsLambda();
     }
 
-    /* Here's all that's left after we take all the needless things from functionAsAnonymousInnerClass() */
-    private static void functionAsLambda() {
-        MyCustomFunctionality pureFunctionalityImplementation = (someInputArgument) -> {
-                System.out.println("This is some functionality implemented as a lambda");
-                return someInputArgument + " from a lambda expression";
-        };
-        pureFunctionalityImplementation.myMethod("Hello");
+    private static void concreteMethodImplementation() {
+        System.out.println("\nconcreteMethodImplementation:");
+        MyCustomClass instanceOfMyClass = new MyCustomClass();
+        String result = instanceOfMyClass.myMethod("Input for concrete implementation");
+        System.out.println("myMethod result is: " + result);
+    }
+
+    private static void abstractMethodImplementation() {
+        System.out.println("\nabstractMethodImplementation:");
+        MyCustomFunctionality instanceOfMyClass = new MyCustomClass();
+        String result = instanceOfMyClass.myMethod("Input for hidden implementation");
+        System.out.println("myMethod result is: " + result);
     }
 
     /* MyCustomClass is not used in this method. Great - it's needless! */
     private static void functionAsAnonymousInnerClass() {
+        System.out.println("\nfunctionAsAnonymousInnerClass:");
         MyCustomFunctionality instanceOfMyClass = new MyCustomFunctionality() {
+            //What's the name of this class? Well there's no class name mentioned. Thus it's an anonymous class.
+            //And also - it (the anonymous class) defined within some other class (Main). Therefore it's an inner class.
+            //That's what an anonymous inner class is!
             @Override
             public String myMethod(String someInputArgument) {
                 System.out.println("This is some functionality implemented within an anonymous inner class");
                 return someInputArgument + " from anonymous inner class";
             }
         };
-        instanceOfMyClass.myMethod("Hello");
+        String result = instanceOfMyClass.myMethod("Input for anonymous inner class");
+        System.out.println("myMethod result is: " + result);
     }
 
-    private static void concreteMethodImplementation() {
-        MyCustomClass instanceOfMyClass = new MyCustomClass();
-        instanceOfMyClass.myMethod("Hello");
-    }
-
-    private static void abstractMethodImplementation() {
-        MyCustomFunctionality instanceOfMyClass = new MyCustomClass();
-        instanceOfMyClass.myMethod("Hello");
+    /* Here's all that's left after we take all the needless things from functionAsAnonymousInnerClass() */
+    private static void functionAsLambda() {
+        System.out.println("\nfunctionAsLambda:");
+        MyCustomFunctionality pureFunctionalityImplementation = (someInputArgument) -> {
+            System.out.println("This is some functionality implemented as a lambda");
+            return someInputArgument + " from a lambda expression";
+        };
+        String result = pureFunctionalityImplementation.myMethod("Hello");
+        System.out.println("myMethod result is: " + result);
     }
 }
--- java2/1_oop/part5/lambdas/src/main/java/MyCustomClass.java
+++ java2/1_oop/part5/lambdas/src/main/java/MyCustomClass.java
@@ -3,6 +3,6 @@ public class MyCustomClass implements MyCustomFunctionality {
     @Override
     public String myMethod(String someInputArgument) {
         System.out.println("This is myMethod with someInputArgument=" + someInputArgument);
-        return someInputArgument + " from MyCustomClass";
+        return someInputArgument + " in MyCustomClass";
     }
 }
