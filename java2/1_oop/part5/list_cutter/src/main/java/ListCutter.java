19a97fc68eb9b43db28ca8b57afd125f283406f8
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-07 21:13:37 +0300

    Java2.OOP.5 Test - List cutter task

---
+++ java2/1_oop/part5/list_cutter/src/main/java/ListCutter.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class ListCutter {
+
+    /**
+     * Write a Java program to extract a portion of an array list.
+     * Also - add needed dependencies and write related unit test
+     *
+     * @param list - list to be cut, like: {"a", "b", "c", "d", "b", "c"}
+     * @param start - index to start cutting the list from, like: 1
+     * @param end - index to cut the list to (inclusively), like: = 3
+     *
+     * @return list that's a sub-list of the original, like ["b", "c", "d"]
+     */
+    public List<String> cut(List<String> list, int start, int end) {
+        return null;
+    }
+}
