ad61ac02fb3a34894fed33dac8791385e8433223
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-07 21:41:47 +0300

    Java2.OOP.5 Test - List filter task

---
+++ java2/1_oop/part5/list_filter/src/main/java/ListFilter.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class ListFilter {
+
+    /**
+     * Write a Java program to count the number of strings in a list
+     * that start with a specific substring using streams
+     * Also add useful dependencies into pom.xml and write a unit test
+     *
+     * For example:
+     * list = {"labas", "laba", "kokteilis", "vakaras", "laukiu"}
+     * subString = "la"
+     * result = 3
+     */
+    public int countStrings(List<String> list, String subString) {
+        return 0;
+    }
+
+    /**
+     * Write a Java program return a sub-list
+     * containing all strings that start with a specific substring using streams
+     *
+     * Also add useful dependencies into pom.xml and write a unit test
+     *
+     * For example:
+     * list = {"labas", "laba", "kokteilis", "vakaras", "laukiu"}
+     * subString = "la"
+     * result = {"labas", "laba", "laukiu"}
+     */
+    public List<String> filterStrings(List<String> list, String subString) {
+        return null;
+    }
+
+
+}
