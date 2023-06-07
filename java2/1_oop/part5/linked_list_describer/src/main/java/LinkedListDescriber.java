e985a6e92273b5c3ffb920f7bb5f593f0371402d
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-07 19:26:25 +0300

    Java2.OOP.5 Test - Add a LinkedListDescriber task

---
+++ java2/1_oop/part5/linked_list_describer/src/main/java/LinkedListDescriber.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class LinkedListDescriber {
+    /**
+     * Write a Java program (and a unit test) to display elements and their positions in a list
+     * @param list linked list containing strings
+     * @return a map, containing the elements and their positions in the linked list
+     *
+     * For example:
+     * given a list = ["", "a", "b", "c", "d", "d", "d", ""]
+     * expect output = {"": [0, 7], "a": [1], "b": [2], "c": [3], "d": [4, 5, 6]}
+     */
+    public Map<String, Integer[]> describe(List<String> list) {
+        return null;
+    }
+}
