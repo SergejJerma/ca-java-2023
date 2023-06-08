8bbc0003ef8754e6ec5dfc27d80ca4bd67402e83
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-08 20:32:21 +0300

    Java2.OOP.5 Test - List joiner task

---
+++ java2/1_oop/part5/list_joiner/src/main/java/ListJoiner.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class ListJoiner {
+
+
+    /**
+     * Write a Java program to join two array lists
+     * @param s1 example: {"Hi", "lovely"}
+     * @param s2 example: {"java", "world"}
+     * @return example: {"Hi", "lovely", "java", "world"}
+     */
+    public List<String> appendToEnd(List<String> s1, List<String> s2) {
+        return null;
+    }
+
+    /**
+     * Write a Java program to join two array lists
+     * @param s1 example: {"Hi", "lovely"}
+     * @param s2 example: {"java", "world"}
+     * @return example: {"Hi", "java", "lovely", "world"}
+     */
+    public List<String> joinSorted(List<String> s1, List<String> s2) {
+        return null;
+    }
+}
