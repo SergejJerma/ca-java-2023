43e5141425547a08a5f7fd7254855db33d3cbef5
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-14 20:29:19 +0300

    Java2.OOP.generics - Convention gor generics

--- java2/1_oop/ja03_generics/generics_intro/src/main/java/Box.java
+++ java2/1_oop/ja03_generics/generics_intro/src/main/java/Box.java
@@ -1,3 +1,12 @@
+/*
+We can use anything instead of "T"
+But it's a matter of convension (culture) to use:
+    K - key
+    V - value
+    E - element / entry
+    T - type
+    N - number
+*/
 interface Box<T> {
 
     void putItem(T item);
