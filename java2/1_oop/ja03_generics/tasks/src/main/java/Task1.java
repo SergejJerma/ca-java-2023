beebbe805f257d6b019860f81c56bd91c0d7ef5d
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-14 21:28:58 +0300

    Java2.OOP.generics - Add task #1

---
+++ java2/1_oop/ja03_generics/tasks/src/main/java/Task1.java
@@ -0,0 +1,17 @@
+/**
+ * 1. Write a Java program to create a generic method
+ * that takes two arrays of the same type
+ * and checks if they have the same elements in the same order.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static <T> boolean areEqual(T[] a, T[] b) {
+        return false;
+    }
+
+
+
+}
