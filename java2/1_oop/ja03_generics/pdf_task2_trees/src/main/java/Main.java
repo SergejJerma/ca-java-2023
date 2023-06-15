9a8674bc57db3208ff6caea4243226764cc6b5e9
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-15 19:36:58 +0300

    Java2.OOP.generics - tree task - add spygliuociuMiskas and berzuMiskas

--- java2/1_oop/ja03_generics/pdf_task2_trees/src/main/java/Main.java
+++ java2/1_oop/ja03_generics/pdf_task2_trees/src/main/java/Main.java
@@ -9,13 +9,30 @@ public class Main {
         Pusis pusis = new Pusis();
         pusis.turi();
 
-        List<Medis> medziai = List.of(new Azuolas(), new Berzas(), new Egle());
-        List<Spygliuotis> spygliuoziai = List.of(new Pusis(), new Kadagys(), new Egle());
-        List<Berzas> berzynas = List.of(new Berzas());
+        List<Medis> medziai = new ArrayList<>();
+        medziai.add(new Azuolas());
+        medziai.add(new Berzas());
+        medziai.add(new Egle());
+
+        List<Spygliuotis> spygliuociai = new ArrayList<>();
+        spygliuociai.add(new Pusis());
+        spygliuociai.add(new Kadagys());
+        spygliuociai.add(new Egle());
+
+        List<Berzas> berzynas = new ArrayList<>();
+        berzynas.add(new Berzas());
 
         ivairusMiskas(medziai);
-        ivairusMiskas(spygliuoziai);
+        ivairusMiskas(spygliuociai);
         ivairusMiskas(berzynas);
+
+        //spygliuociuMiskas(medziai);
+        spygliuociuMiskas(spygliuociai);
+        //spygliuociuMiskas(berzynas);
+
+        //berzuMiskas(medziai);
+        //berzuMiskas(spygliuoziai);
+        berzuMiskas(berzynas);
     }
 
     // Generics (generic method)
@@ -36,6 +53,28 @@ public class Main {
         Medis pirmasMedis = medziai.get(0);
         pirmasMedis.turi();
         //medziai.add(pirmasMedis);   // <-- generic'u silpnoji vieta - negalime prideti i sarasa: "Required type: T; Provided Medis"
+    }
+
+    static <T extends Spygliuotis> void spygliuociuMiskas(List<T> spygliuociai) {
+        spygliuociai.forEach(spygliuotis -> spygliuotis.turi()); //lambda pavyzdys
+
+        T pirmasSpygliuotisT = spygliuociai.get(0);
+        pirmasSpygliuotisT.turi();
+        spygliuociai.add(pirmasSpygliuotisT);
+
+        Spygliuotis pirmasSpygliuotis = spygliuociai.get(0);
+        pirmasSpygliuotis.turi();
+    }
+
+    static <T extends Berzas> void berzuMiskas(List<T> berzai) {
+        berzai.forEach(berzas -> berzas.turi()); //lambda pavyzdys
+
+        T pirmasBerzasT = berzai.get(0);
+        pirmasBerzasT.turi();
+        berzai.add(pirmasBerzasT);
+
+        Berzas pirmasBerzas = berzai.get(0);
+        pirmasBerzas.turi();
     }
 
 
