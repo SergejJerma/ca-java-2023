e0a591de7a5dc85f3d8964ff1569d67e3b628d79
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-15 20:30:51 +0300

    Java2.OOP.generics - type checking

--- java2/1_oop/ja03_generics/slides2_task/src/main/java/Calculator.java
+++ java2/1_oop/ja03_generics/slides2_task/src/main/java/Calculator.java
@@ -1,8 +1,18 @@
-public class Calculator {
+public  class Calculator <T extends Number> {
 
+    private T varA;
+    private T varB;
 
+    public Calculator(T varA, T varB) {
+        this.varA = varA;
+        this.varB = varB;
+    }
 
-    public <T extends Number> Number sum(T a, T b) {
+    public  Number sum(T a, T b) {
         return a.doubleValue() + b.doubleValue();
+    }
+
+    public  Number sum() {
+        return varA.doubleValue() + varB.doubleValue();
     }
 }
--- java2/1_oop/ja03_generics/slides2_task/src/main/java/Main.java
+++ java2/1_oop/ja03_generics/slides2_task/src/main/java/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        //raw types (not recommended)
+        Calculator calculator = new Calculator(1, 5.9f);
+
+        System.out.println(calculator.sum());
         System.out.println(calculator.sum(1, 5.9f));
-        System.out.println(calculator.sum(Integer.valueOf(1), Float.valueOf(5.9f)));
         //System.out.println(calculator.sum("1", "5.9f"));  //calculator.sum only accepts subtypes of Number
+
+
+        //type checking
+        //Calculator<Integer> integerCalculator = new Calculator<>(1, 2.0); //compile time error
+        Calculator<Integer> integerCalculator = new Calculator<>(1, 2);
+
+
     }
 }
