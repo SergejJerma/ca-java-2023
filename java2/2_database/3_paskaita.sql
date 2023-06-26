6a4ae99e7411664cd61e7a6ba11533dbf852e31e
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-26 23:26:24 +0300

    Java2.databases - update 3_paskaita.sql

--- java2/2_database/3_paskaita.sql
+++ java2/2_database/3_paskaita.sql
@@ -84,16 +84,20 @@ JOIN projektai ON projektas = projektai.nr;
 
 -- LEFT OUTER JOIN
 -- Darbuotojai ir jų dalyvavimas projekte Nr. 1:
-SELECT pavarde, statusas, valandos
+SELECT pavarde, nr vykdytojo_nr, statusas, valandos, projektas, vykdytojas
 FROM vykdytojai 
-	LEFT OUTER JOIN vykdymas ON vykdytojas = nr
-WHERE projektas = 1;
+	LEFT OUTER JOIN vykdymas ON vykdytojas = nr;
+
+-- Rezultate bus tik tų autorių pavardės kurie yra abiejose lentelese (vykdytojai ir vykdymas)
+-- vykdytojo Nr. 5 nebus nes jo nėra  lentelėje "Vykdymas".
+SELECT pavarde, statusas, valandos, projektas, vykdytojas, nr
+FROM vykdytojai 
+	inner JOIN vykdymas ON vykdytojas = nr;
 
 -- Rezultatas nesikeičia taikant RIGHT OUTER JOIN ir  sukeičiant lenteles vietomis:
-SELECT pavarde, statusas, valandos
-FROM vykdymas
-	RIGHT OUTER JOIN vykdytojai ON vykdytojas = nr
-WHERE projektas = 1;
+SELECT pavarde, nr vykdytojo_nr, statusas, valandos, projektas, vykdytojas
+FROM  vykdymas 
+	RIGHT OUTER JOIN vykdytojai ON vykdytojas = nr;
 
 -- Struktūrinės  užklausos – (Structured QL).
 -- Pavardės vykdytojų, dalyvaujančių projekte Nr. 1:
@@ -111,6 +115,14 @@ WHERE nr IN (
     WHERE projektas IN (SELECT nr FROM projektai WHERE svarba = 'Ypatinga')
 );
 
+-- Tas pats, tik naudojant join
+select pavarde
+from vykdytojai
+	inner join vykdymas on vykdymas.vykdytojas = vykdytojai.nr
+	inner join projektai on projektai.nr = vykdymas.projektas
+where svarba = 'Ypatinga' ;
+
+
 -- Didelis dalinių užklausų kiekis, nereiškia gero stiliaus.
 -- Vykdytojai, dalyvaujantys bent viename didelės svarbos projekte gali būti  išrinkti ir taip:
 SELECT DISTINCT pavarde, projektas, svarba
@@ -120,4 +132,22 @@ WHERE
     AND vykdytojas = vykdytojai.nr  
     AND svarba = 'Aukšta';
 
+-- 3. Išrinkite visus projekto Registrų Centras vykdytojus dirbančius Testavimo skyriuje.
+use schema darbuotojai;
+select * from DARBUOTOJAS
+	inner join PROJEKTAS on DARBUOTOJAS.PROJEKTAS_ID = PROJEKTAS.ID
+ where SKYRIUS_PAVADINIMAS = 'Testavimo'
+ and PROJEKTAS.PAVADINIMAS='Registrų Centras';
 
+-- 10. 1# punkto užklausą pataisykite taip, kad rodytų tik tuos vardus ir projektų pavadinimus
+--kuriuose dirba daugiau nei 4 darbuotojai.
+SELECT D.VARDAS, D.PAVARDE, P.PAVADINIMAS
+FROM DARBUOTOJAS D
+    JOIN PROJEKTAS P ON P.ID = D.PROJEKTAS_ID
+WHERE PROJEKTAS_ID IN
+(
+    SELECT PROJEKTAS_ID
+    FROM DARBUOTOJAS
+    GROUP BY PROJEKTAS_ID
+    HAVING COUNT(PROJEKTAS_ID) > 4
+);
