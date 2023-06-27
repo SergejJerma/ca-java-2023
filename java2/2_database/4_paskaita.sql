08a7248eb6530c504081cda640bfd6f6e574c698
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-06-27 20:37:05 +0300

    Java2.databases - update 4_paskaita.sql

--- java2/2_database/4_paskaita.sql
+++ java2/2_database/4_paskaita.sql
@@ -75,5 +75,50 @@ insert into persons (p_id, last_name) values (1, 'lasName2');
 insert into persons (p_id, last_name) values (2, 'lasName');
 
 
- 
- 
+
+create schema orders;
+
+CREATE TABLE persons
+(
+    p_id      INT          NOT NULL PRIMARY KEY,
+    last_name  VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255),
+    address   VARCHAR(255),
+    city      VARCHAR(255)
+);
+
+
+
+-- -------------------------
+-- foreign key
+-- -------------------------
+drop table if exists orders;
+CREATE TABLE orders
+(
+    o_id    INT NOT NULL PRIMARY KEY,
+    order_no INT NOT NULL,
+    p_id    INT,
+    CONSTRAINT fk_per_orders FOREIGN KEY (p_id) #constraint galima sukurti kuriant lentele
+        REFERENCES persons(p_id)
+);
+
+#constraint galima sukurti (prideti) jau egzistuojanciai lentelei
+ALTER TABLE orders ADD FOREIGN KEY (p_id)
+    REFERENCES persons(p_id);
+
+
+-- constraint trynimas
+ALTER TABLE orders DROP CONSTRAINT fk_per_orders;
+
+
+-- -------------------------
+-- indeksai
+-- -------------------------
+CREATE INDEX mano_indeksas ON persons (first_name, last_name);
+
+-- indekso trynimas (reikia ir lenteles pavadinima nurodyti)
+DROP INDEX mano_indeksas ON persons;
+
+-- indeksai susija su lentele
+SHOW INDEXES FROM persons;
+
