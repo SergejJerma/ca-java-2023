d1d92ba639757fe210d151adbb77879a9c31632f
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-08-18 20:47:08 +0300

    Java2.spring boot - add PathVariable

--- java2/3_spring/thymeleaf_demo/src/main/java/com/codeacademy/thymeleaf_demo/HelloWorldController.java
+++ java2/3_spring/thymeleaf_demo/src/main/java/com/codeacademy/thymeleaf_demo/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.codeacademy.thymeleaf_demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class HelloWorldController {
     @GetMapping("/world")
     public String sayHello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/world/{name}")
+    public String sayHelloToMe(@PathVariable String name) {
+        return "Hello World!" + name;
     }
 
 }
