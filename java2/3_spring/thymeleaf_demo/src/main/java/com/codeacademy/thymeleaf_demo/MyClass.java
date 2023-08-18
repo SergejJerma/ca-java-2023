2314e1b0fd9548b829e60bce3a3906129f9d1b9c
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-08-18 21:43:37 +0300

    Java2.spring boot - add dynamic thymeleaf template

---
+++ java2/3_spring/thymeleaf_demo/src/main/java/com/codeacademy/thymeleaf_demo/MyClass.java
@@ -0,0 +1,11 @@
+package com.codeacademy.thymeleaf_demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MyClass {
+    private String myName;
+
+}
--- java2/3_spring/thymeleaf_demo/src/main/java/com/codeacademy/thymeleaf_demo/ThymeleafController.java
+++ java2/3_spring/thymeleaf_demo/src/main/java/com/codeacademy/thymeleaf_demo/ThymeleafController.java
@@ -1,7 +1,9 @@
 package com.codeacademy.thymeleaf_demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +12,15 @@ public class ThymeleafController {
 
     @GetMapping
     public String thymeleaf() {
+        return "myCustomHtml";
+    }
+
+    @GetMapping("/dynamicHtml/{name}")
+    public String thymeleaf(@PathVariable String name, Model model) {
+        MyClass myClassInstance = new MyClass();
+        myClassInstance.setMyName(name);
+
+        model.addAttribute("paramName", myClassInstance);
         return "myCustomHtml";
     }
 }
