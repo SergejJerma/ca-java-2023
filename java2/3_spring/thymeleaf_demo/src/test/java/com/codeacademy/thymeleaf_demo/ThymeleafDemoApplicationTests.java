8d712cb05335640333c418437a0e82a26f517e43
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-08-18 19:59:32 +0300

    Java2.spring boot - add project from Spring initializr

---
+++ java2/3_spring/thymeleaf_demo/src/test/java/com/codeacademy/thymeleaf_demo/ThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeacademy.thymeleaf_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
