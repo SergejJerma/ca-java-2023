0ca3895f43ba20c95059e22f71c4faeb82e5646c
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-08-18 20:50:21 +0300

    Java2.spring boot - add RequestParam

--- java2/3_spring/thymeleaf_demo/src/main/java/com/codeacademy/thymeleaf_demo/HelloWorldController.java
+++ java2/3_spring/thymeleaf_demo/src/main/java/com/codeacademy/thymeleaf_demo/HelloWorldController.java
@@ -1,21 +1,26 @@
 package com.codeacademy.thymeleaf_demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
 
+    //http://localhost:8080/hello/world
     @GetMapping("/world")
     public String sayHello() {
         return "Hello World!";
     }
 
+    //http://localhost:8080/hello/world/tomas
     @GetMapping("/world/{name}")
     public String sayHelloToMe(@PathVariable String name) {
+        return "Hello World!" + name;
+    }
+
+    //http://localhost:8080/hello/world/byRequestParam?name=tomas_zvinys
+    @GetMapping("/world/byRequestParam")
+    public String sayHelloToMeUsingRequestParam(@RequestParam String name) {
         return "Hello World!" + name;
     }
 
