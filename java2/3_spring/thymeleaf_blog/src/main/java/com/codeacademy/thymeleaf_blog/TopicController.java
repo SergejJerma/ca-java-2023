c40e29c084f1be16d81214107e201d597666bbd5
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-08-22 20:46:43 +0300

    Java2.spring boot - add links between pages

--- java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/TopicController.java
+++ java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/TopicController.java
@@ -20,20 +20,15 @@ public class TopicController {
 
     @GetMapping
     public String getTopics(Model model) {
-
-//        List<Topic> topics = List.of(
-//                new Topic("Most popular films", "More info"),
-//                new Topic("Top rated films", "More info"),
-//                new Topic("Latest movies", "More info"),
-//                new Topic("Most expensive movies", "More info")
-//        );
-
         List<Topic> topics = topicService.getAllTopics();
-
         model.addAttribute("topics", topics);
-        model.addAttribute("newTopic", new Topic());
-
         return "topics";
+    }
+
+    @GetMapping("/add")
+    public String getAddTopicForm(Model model) {
+        model.addAttribute("newTopic", new Topic());
+        return "addTopic";
     }
 
     @PostMapping
