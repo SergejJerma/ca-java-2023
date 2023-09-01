8c0a9bbd29458afe6aa84209beb49c053c63051e
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-09-01 19:27:08 +0300

    Refactor TopicService

--- java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/service/TopicService.java
+++ java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/service/TopicService.java
@@ -11,22 +11,14 @@ import java.util.List;
 @Service
 public class TopicService {
 
-    private TopicRepository topicRepository;
+    private final TopicRepository topicRepository;
 
     public TopicService(TopicRepository topicRepository) {
         this.topicRepository = topicRepository;
     }
 
-    public List<Topic> getAllTopics() {
-        return topicRepository.findAll();
-    }
-
     public Topic addNewTopic(Topic newTopic) {
         return topicRepository.save(newTopic);
-    }
-
-    public List<Topic> findTopicsByTitle(String topicTitle) {
-        return topicRepository.findByTitle(topicTitle);
     }
 
     public Topic getTopic(Long id) {
