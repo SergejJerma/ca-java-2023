d74a2828a1ff3b380ceffd9e8946a3895d83796c
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-09-04 21:15:32 +0300

    Try using values from property files, depending on active profile

--- java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/controller/OnlyTestController.java
+++ java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/controller/OnlyTestController.java
@@ -1,5 +1,6 @@
 package com.codeacademy.thymeleaf_blog.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,9 @@ public class OnlyTestController {
 
     private final Environment environment;
 
+    @Value("${my.custom.environment.property}")
+    private String environmentProperty;
+
     public OnlyTestController(Environment environment) {
         this.environment = environment;
     }
@@ -22,8 +26,8 @@ public class OnlyTestController {
     @GetMapping("/profiles")
     public String helloProfiles() {
         String allActiveProfiles = Arrays.toString(environment.getActiveProfiles());
-        return "Some info %s about currently active profiles that should not be visible in PROD"
-                .formatted(allActiveProfiles);
+        return "Some info %s about currently active profiles that should not be visible in PROD, like %s"
+                .formatted(allActiveProfiles, this.environmentProperty);
     }
 
 }
