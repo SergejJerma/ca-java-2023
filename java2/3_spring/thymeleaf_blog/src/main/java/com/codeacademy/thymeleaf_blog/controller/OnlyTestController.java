df711713845fed50bccea92c4bd5175986604e53
Tomas Zvinys <tomas.zvinys@gmail.com>
2023-09-05 18:28:02 +0300

    Replace use property from custom properties file using @PropertySource

--- java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/controller/OnlyTestController.java
+++ java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/controller/OnlyTestController.java
@@ -2,6 +2,7 @@ package com.codeacademy.thymeleaf_blog.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
+@PropertySource("classpath:mySecrets.properties")
 @Profile(value = "!prodProfile")
 @RestController
 @RequestMapping("/hello")
@@ -19,15 +21,20 @@ public class OnlyTestController {
     @Value("${my.custom.environment.property}")
     private String environmentProperty;
 
-    public OnlyTestController(Environment environment) {
+//    @Value("${some.value.from.property.source}")
+    private final String someValue;
+
+    public OnlyTestController(Environment environment, @Value("${some.value.from.property.source}") String prop) {
         this.environment = environment;
+        this.someValue = prop;
     }
 
     @GetMapping("/profiles")
     public String helloProfiles() {
         String allActiveProfiles = Arrays.toString(environment.getActiveProfiles());
-        return "Some info %s about currently active profiles that should not be visible in PROD, like %s"
+        String message = "Some info %s about currently active profiles that should not be visible in PROD, like %s"
                 .formatted(allActiveProfiles, this.environmentProperty);
+        return message.concat("\n some value from propertySource: %s".formatted(someValue));
     }
 
 }
