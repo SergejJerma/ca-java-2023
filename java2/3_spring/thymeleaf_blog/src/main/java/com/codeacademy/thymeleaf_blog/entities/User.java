4fcc57b72b153d6f5b7024d59a517c6e773cf1a7
Serzas <j_sergej@yahoo.com>
2023-09-06 20:36:54 +0300

    added custom annotation

--- java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/entities/User.java
+++ java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/entities/User.java
@@ -1,5 +1,6 @@
 package com.codeacademy.thymeleaf_blog.entities;
 
+import com.codeacademy.thymeleaf_blog.util.PhoneNumber;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,5 +33,6 @@ public class User {
 
     private String country;
 
+    @PhoneNumber(message = "Phone number is not valid.")
     private String phoneNumber;
 }
---
+++ java2/3_spring/thymeleaf_blog/src/main/java/com/codeacademy/thymeleaf_blog/util/PhoneNumberType.java
@@ -0,0 +1,5 @@
+package com.codeacademy.thymeleaf_blog.util;
+
+public enum PhoneNumberType {
+    FULL
+}
